9f034ec5d30eca117696d68d33c35a0cfd3b07f2
vlmna13 <vlmna13@gmail.com>
2024-05-14 12:17:10 +0300

    like-objects

---
+++ 7k/like-objects.js
@@ -0,0 +1,11 @@
+DESCRIPTION:
+// For this exercise you should create a JavaScript class like object called "Animal" that takes in "name" and "type" arguments. It should have a toString method that returns a human readable string indicating the argument information passed in. It should also allow the name property to be set.
+
+class Animal{
+    constructor (name,type){
+    this.name=name;
+    this.type=type;
+    }
+    toString(){
+    return `${this.name} is a ${this.type}`}
+    }
