ff773edf905a6fc340779bcc4e1bbe80a1f59e96
vlmna13 <vlmna13@gmail.com>
2024-05-14 12:12:50 +0300

    makeClass

---
+++ 7k/make-Class.js
@@ -0,0 +1,13 @@
+// Give me the power to create a similar class like this:
+
+// const Animal = makeClass("name","species","age","health","weight","color") 
+
+function makeClass(...properties) {
+    return class {
+      constructor(...args) {
+        properties.forEach((property, index) => {
+          this[property] = args[index];
+        });
+      }
+    };
+  }
